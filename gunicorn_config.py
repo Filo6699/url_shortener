074f9bf3d8bb1314323536958bb8bae236972408
Filo6699 <karka2-6@rambler.ru>
2023-11-13 10:01:06 +0600

    rickrolling bots, im angy

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 
 bind = "0.0.0.0:5000"  # Change the port if needed
-workers = 3  # Adjust the number of workers based on your requirements
-keyfile = os.getenv("SSL_PRIVATE_KEY_PATH")
-certfile = os.getenv("SSL_CERTIFICATE_PATH")
+workers = 1  # Adjust the number of workers based on your requirements
+if os.getenv("SSL") == 'on':
+    keyfile = os.getenv("SSL_PRIVATE_KEY_PATH")
+    certfile = os.getenv("SSL_CERTIFICATE_PATH")
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,20 @@
+from flask import request, redirect
 from main import app, init, shutdown
 import atexit
+
+class RedirectMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        request_path = environ.get("PATH_INFO", "")
+        if "wp-includes" in request_path:
+            # Redirect the request to a different URL
+            return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ", code=302)(environ, start_response)
+
+        return self.app(environ, start_response)
+
+app.wsgi_app = RedirectMiddleware(app.wsgi_app)
 
 atexit.register(shutdown)
 init()
