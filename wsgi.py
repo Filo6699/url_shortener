9ad40c56ea444a641673b18f9d9947f699e31862
Filo6699 <karka2-6@rambler.ru>
2023-11-13 10:05:19 +0600

    and also i don't like .env bots

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ class RedirectMiddleware:
 
     def __call__(self, environ, start_response):
         request_path = environ.get("PATH_INFO", "")
-        if "wp-includes" in request_path:
+        if "wp-includes" in request_path or ".env" in request_path:
             # Redirect the request to a different URL
             return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ", code=302)(environ, start_response)
 
